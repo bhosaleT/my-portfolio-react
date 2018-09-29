30b1e80e86f6697b1f647804a62ecefb153e9e16
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-29 23:15:41 +0530

    updated CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,11 @@ class Header extends React.Component {
             src="https://static.tvtropes.org/pmwiki/pub/images/eric-cartman_380.jpg"
             alt="Tejas-Bhosale"
           />
-          <div>
+          <div className="info">
             <h2 className="name">Tejas Bhosale</h2>
             <p className="title">
-              Full Stack Developer <span> / </span> Game Developer
+              Full Stack Developer <span className="divider"> || </span> Game
+              Developer
             </p>
           </div>
         </div>
