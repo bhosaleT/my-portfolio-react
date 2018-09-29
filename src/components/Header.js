b8593e086940fde8792db84150c001b5bf3a8c0c
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-29 18:21:34 +0530

    updated css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import React from "react";
+import Selector from './Selector';
 
 class Header extends React.Component {
   render() {
-    const profileImage = "../images/eric-cartman_380.jpg";
     return (
-      <div className="header">
-        <img
-          className="profile-image"
-          src="https://static.tvtropes.org/pmwiki/pub/images/eric-cartman_380.jpg"
-          alt="Tejas-Bhosale"
-        />
-        <div>
-          <h2 className="name">Tejas Bhosale</h2>
-          <p className="title">Full Stack Developer || Game Developer</p>
+      <div>
+        <div className="header">
+          <img
+            className="profile-image"
+            src="https://static.tvtropes.org/pmwiki/pub/images/eric-cartman_380.jpg"
+            alt="Tejas-Bhosale"
+          />
+          <div>
+            <h2 className="name">Tejas Bhosale</h2>
+            <p className="title">Full Stack Developer || Game Developer</p>
+          </div>
+          <Selector />
         </div>
+        <hr className="hr"/>
       </div>
     );
   }
---
+++ src/components/Selector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Selector extends React.Component{
+    render(){
+        return(
+            <div>
+            SELECTOR
+            </div>
+        )
+    }
+}
+
+export default Selector;
