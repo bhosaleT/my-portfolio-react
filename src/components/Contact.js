a458680495985dba72fd9223fb96ab6119f81eb8
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-10-26 21:53:02 +0530

    updated css for map in contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,174 +14,95 @@ class Contact extends React.Component {
   };
 
   initMap = () => {
-      var styles = [
+    var styles = [
+      {
+        featureType: "administrative",
+        elementType: "labels.text.fill",
+        stylers: [
           {
-              "featureType": "all",
-              "elementType": "labels.text.fill",
-              "stylers": [
-                  {
-                      "saturation": 36
-                  },
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 40
-                  }
-              ]
-          },
-          {
-              "featureType": "all",
-              "elementType": "labels.text.stroke",
-              "stylers": [
-                  {
-                      "visibility": "on"
-                  },
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 16
-                  }
-              ]
-          },
-          {
-              "featureType": "all",
-              "elementType": "labels.icon",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative",
-              "elementType": "geometry.fill",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 20
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative",
-              "elementType": "geometry.stroke",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 17
-                  },
-                  {
-                      "weight": 1.2
-                  }
-              ]
-          },
-          {
-              "featureType": "landscape",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 20
-                  }
-              ]
-          },
-          {
-              "featureType": "poi",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 21
-                  }
-              ]
-          },
-          {
-              "featureType": "road.highway",
-              "elementType": "geometry.fill",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 17
-                  }
-              ]
-          },
-          {
-              "featureType": "road.highway",
-              "elementType": "geometry.stroke",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 29
-                  },
-                  {
-                      "weight": 0.2
-                  }
-              ]
-          },
-          {
-              "featureType": "road.arterial",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 18
-                  }
-              ]
-          },
-          {
-              "featureType": "road.local",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 16
-                  }
-              ]
-          },
-          {
-              "featureType": "transit",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 19
-                  }
-              ]
-          },
-          {
-              "featureType": "water",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#000000"
-                  },
-                  {
-                      "lightness": 17
-                  }
-              ]
+            color: "#444444"
           }
-      ]
-
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#f2f2f2"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100
+          },
+          {
+            lightness: 45
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#dbdbdb"
+          },
+          {
+            visibility: "on"
+          }
+        ]
+      }
+    ];
     var contentString = `<div>
   <h3 class="map-heading">I am from here</h3>
   <p class="map-info"> Kalyan, Thane, Mumbai, Maharashtra 421301</p>
@@ -196,16 +117,16 @@ class Contact extends React.Component {
         lat: 19.2183,
         lng: 72.9781
       },
-      zoom: 11,
+      zoom: 12,
       styles: styles,
       disableDefaultUI: true,
-    //   gestureHandling: "greedy",
+      //   gestureHandling: "greedy",
       mapTypeControl: false
     });
 
     var marker = new window.google.maps.Marker({
       position: {
-        lat: 19.228,
+        lat: 19.2219,
         lng: 72.9791
       },
       map: map,
