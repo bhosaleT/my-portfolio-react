a0419c5f3095ee8e2a011e41e934f94cf8a622c7
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-14 18:54:10 +0530

    updated projects component

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
-class ProjectItem extends React.Component{
-    render(){
-        return(
-            <div>
-            <h1>{this.props.project.name}</h1>
-            <p>{this.props.project.descShort}</p>
-            </div>
-        )
-    }
+class ProjectItem extends React.Component {
+  render() {
+    return (
+      <div className="project">
+        <h1 className="project-title">{this.props.project.name}</h1>
+        <p className="project-desc">{this.props.project.descShort}</p>
+        <div className="tag-div">
+          <ul className="tags">
+            {this.props.project.tags.map(tag => (
+              <li className="tag-item">#{tag}</li>
+            ))}
+          </ul>
+        </div>
+        <button className="button">View More</button>
+      </div>
+    );
+  }
 }
 
 export default ProjectItem;
--- src/projects.js
+++ src/projects.js
@@ -1,28 +1,39 @@
 const projectElements = {
-    project1: {
-        name: "When In Mumbai",
-        descShort: "First time in Mumbai? This is where you should go"
-    },
-    project2: {
-        name: "Pocket Money",
-        descShort: "Note down all those pesky expenses and keep track of your money!"
-    },
-    project3: {
-        name: "Thats Delicious",
-        descShort: "Every foodies dream! Share your favourite food junction with the world!"
-    },
-    project4: {
-        name: "Bookish",
-        descShort: "We know you got Books for Days! let us help you manage them!"
-    },
-    project5: {
-        name: "Advanced CSS",
-        descShort: "Three beautifully designed websites using everything that CSS has to offer!"
-    },
-    project6: {
-        name: "Cinema Wars",
-        descShort: "Your favourite movie is better than your bff's use this site to prove it!"
-    }
-}
+  project1: {
+    name: "When In Mumbai",
+    descShort: "First time in Mumbai? This is where you should go first!",
+    // descLong: "A Google Maps based website that shows "
+    tags: ["React.js", "AJAX", "AXIOS", "ES6", "SASS"]
+  },
+  project2: {
+    name: "Pocket Money",
+    descShort:
+      "Note down all those pesky expenses and keep track of your money!",
+    tags: ["React.js", "Redux", "Firebase", "SASS", "ES6", "Jest"]
+  },
+  project3: {
+    name: "Thats Delicious",
+    descShort:
+      "Every foodies dream! Share your favourite food junction with the world!",
+    tags: ["NODE.js", "Express", "MongoDB", "ES6", "SASS"]
+  },
+  project4: {
+    name: "Bookish",
+    descShort: "We know you got Books for Days! let us help you manage them!",
+    tags: ["React.js", "CSS", "ES6", "Jest"]
+  },
+  project5: {
+    name: "Advanced CSS",
+    descShort:
+      "Three beautifully designed websites using everything that CSS has to offer!",
+    tags: ["SASS", "CSS_GRID", "FLEXBOX", "ES6", "ANIMATIONS"]
+  },
+  project6: {
+    name: "Cinema Wars",
+    descShort:
+      "Your favourite movie is better than your bff's use this site to prove it!",
+    tags: ["React.js", "CSS", "MovieDB API", "ES6", "Jest"]
+  }
+};
 
 export default projectElements;
