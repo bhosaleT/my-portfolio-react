c9cdc8e8e17bdcb53ef1f1a0cbe4909e04b05b4a
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-12-03 11:38:51 +0530

    updated about info

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,7 @@ const About = () => (
     />
     <div className="about__text">
       <p className="about-text">
-        I'm a <span className="highlight">Full Stack Developer</span> from
-        Mumbai, India. I recently graduated from Datta Meghe College of
-        Engineering in the field of{" "}
-        <span className="highlight">Computer Engineering</span> . I'm
-        comfortable with the full stack which includes front-end frameworks like{" "}
-        <span className="highlight">React.js</span> and back-end frameworks like{" "}
-        <span className="highlight">Node.js</span> . Creativity and Always
-        Learning are my major plus points and I'm passionate about building
-        excellent websites and discovering new ways to get the job done!
+        I'm a newly graduated web developer offering enthusiasm and understanding of Front-end frameworks like React, Redux and advanced CSS techniques and back-end frameworks like Node.js and mongoDB. I will bring to the table the ability to design and develop beautiful and functional websites using the latest technologies and guidelines. 
       </p>
 
       <a className="button" href={require("../resume/resume.pdf")} download>
